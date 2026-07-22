df8b0e6b234b1b42b40a1eac123c44e4400c6bc8
mescobar996 <matialeescobar96@gmail.com>
2026-07-22 01:56:28 -0300

    feat(selenium-plugin): add barrel export

---
+++ selenium-plugin/src/index.ts
@@ -0,0 +1,3 @@
+export { HealifySeleniumPlugin } from './plugin'
+export type { HealifySeleniumOptions, HealingEvent, HealingEventType } from './types'
+export { DEFAULT_CONFIDENCE_THRESHOLD } from './types'
